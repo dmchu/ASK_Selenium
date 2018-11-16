0cb2df054baca53f9b0c4f9b5b11dc0347331494
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2018-11-16 06:05:36 -0800

    created base test case

---
+++ ASK/fixtures/base.py
@@ -0,0 +1,29 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+from pages.login_page import LoginPage
+from parameters.parameters import *
+
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path="../browsers/chromedriver")
+        self.wait = WebDriverWait(self.driver, 20)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+class TeacherLoginTestCase(BaseTestCase):
+    def setUp(self):
+        super(TeacherLoginTestCase, self).setUp()
+        self.login = LoginPage(self.driver)
+        self.login.goto_page()
+        self.login.login_as_teacher(email_teacher, password_teacher)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
