a9cb9f6cc5317c4a9a7ba775a68c12cff41fdf93
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2018-11-23 14:27:24 -0800

    Update login_page.py

--- ASK/pages/login_page.py
+++ ASK/pages/login_page.py
@@ -6,8 +6,8 @@ from parameters.parameters import *
 from pages.base_page import BasePage
 
 locators = {
-    "login_email": {"by": By.ID, "locator": "mat-input-0"},
-    "login_password": (By.ID, "mat-input-1"),
+    "login_email": {"by": By.XPATH, "locator": "//input[@formcontrolname='email']"},
+    "login_password": (By.XPATH, "//input[@type='password']"),
     "submit_button": (By.CSS_SELECTOR, "button[type='submit']"),
     "teacher_role_text": (By.XPATH, "// div[@class = 'info']/p[contains(text(),'TEACHER')]"),
     "student_role_text": (By.XPATH, "// div[@class = 'info']/p[contains(text(),'STUDENT')]")
@@ -21,17 +21,15 @@ class LoginPage(BasePage):
         self.page_url = DOMAIN + "/login"
 
     def login_as_teacher(self,email_teacher,password_teacher):
-        driver = self.driver
-        wait = self.wait
-        driver.find_element(locators["login_email"]["by"], locators["login_email"]["locator"]).send_keys(email_teacher)
-        driver.find_element(locators["login_password"][0], locators["login_password"][1]).send_keys(password_teacher)
-        driver.find_element(locators["submit_button"][0], locators["submit_button"][1]).click()
-        wait.until(EC.presence_of_element_located(locators["teacher_role_text"]))
+        self.driver.find_element(locators["login_email"]["by"], locators["login_email"]["locator"]).send_keys(email_teacher)
+        self.driver.find_element(locators["login_password"][0], locators["login_password"][1]).send_keys(password_teacher)
+        self.driver.find_element(locators["submit_button"][0], locators["submit_button"][1]).click()
+        self.wait.until(EC.presence_of_element_located(locators["teacher_role_text"]))
 
-    def login_as_student(self,email,password):
-        driver = self.driver
-        wait = self.wait
-        driver.find_element(locators["login_email"]["by"], locators["login_email"]["locator"]).send_keys(email)
-        driver.find_element(locators["login_password"][0], locators["login_password"][1]).send_keys(password)
-        driver.find_element(locators["submit_button"][0], locators["submit_button"][1]).click()
-        wait.until(EC.presence_of_element_located(locators["student_role_text"]))
+    def login_as_student(self,email_student,password_student):
+        self.driver.find_element(locators["login_email"]["by"], locators["login_email"]["locator"]).send_keys(email_student)
+        self.driver.find_element(locators["login_password"][0], locators["login_password"][1]).send_keys(password_student)
+        self.driver.find_element(locators["submit_button"][0], locators["submit_button"][1]).click()
+        self.wait.until(EC.presence_of_element_located(locators["student_role_text"]))
+
+
