c241f7ee84778134a5a3abb868479a65900c5524
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2018-12-18 14:42:04 -0800

    Update quiz_page.py
    Added Locators
    Code refactored

--- ASK/pages/quiz_page.py
+++ ASK/pages/quiz_page.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,6 +10,25 @@ from fixtures.params import DOMAIN
 from pages.base_page import BasePage
 from steps.common import click, type, wait_until
 
+
+
+locators = {
+    "create_quiz_btn": (By.XPATH, "//span[text() ='Create New Quiz']/.."),
+    "quiz_name_field": (By.XPATH, "//label[contains(text(), 'Title Of The Quiz *')]/../../input"),
+    "add_question_btn": (By.CSS_SELECTOR, "div.controls.ng-star-inserted>button"),
+    "label_textual": (By.XPATH, "//*[contains(text(), 'new empty question')]/../../..//div[contains(text(), 'Textual')]/.."),
+    "label_single_choice": (By.XPATH, "//*[contains(text(), 'new empty question')]/../../..//div[contains(text(), 'Single-Choice')]/.."),
+    "label_multi_choice": (By.XPATH, "//*[contains(text(), 'new empty question')]/../../..//div[contains(text(), 'Multiple-Choice')]/.."),
+    "question_field_locator" : (By.XPATH, "//*[contains(text(), 'new empty question')]/../../..//textarea[@placeholder='Question *']"),
+    "question_choice_field": (By.XPATH, "//*[contains(text(), '{}')]/../../..//*[@placeholder='Option {}*']"),
+    "single_choice_radio_btn" : (By.XPATH, "//*[contains(text(), '{}')]/../../..//*[@placeholder='Option {}*']/../../../../..//mat-radio-button"),
+    "multi_choice_ch_box" : (By.XPATH, "//*[contains(text(), '{}')]/../../..//*[@placeholder='Option {}*']/../../../../../mat-checkbox"),
+    "save_quiz_btn" : (By.XPATH, "//button/*[contains(text(),'Save')]"),
+    "quiz_locator" : (By.XPATH, "//ac-quizzes-list//div[@class = 'quizzes']//*[contains(text(),'{}')]")
+}
+
+
+
 class QuizPage(BasePage):
 
     def __init__(self, driver):
@@ -15,89 +36,64 @@ class QuizPage(BasePage):
         self.page_url = DOMAIN + "/quizzes"
 
     def create_new_quizz(self, quiz_name):
-        click(self.driver, By.PARTIAL_LINK_TEXT, "Quizzes")
-        wait_until(self.wait, self.driver, By.CSS_SELECTOR, "a[href='#/quiz-builder']")
-        click(self.driver, By.PARTIAL_LINK_TEXT, "Create New Quiz")
-        type(self.driver, By.TAG_NAME, "input", quiz_name)
-        self.quiz_name = quiz_name
+        self.wait.until(EC.element_to_be_clickable(
+            (locators["create_quiz_btn"][0], locators["create_quiz_btn"][1]))).click()
+        self.wait.until(EC.presence_of_element_located(
+            (locators["quiz_name_field"][0], locators["quiz_name_field"][1]))).send_keys(quiz_name)
 
     def add_new_question(self):
-        by = By.CSS_SELECTOR
-        locator = "div.controls.ng-star-inserted>button"
-        wait_until(self.wait, self.driver, by, locator)
-        click(self.driver, by, locator)
+        self.wait.until(EC.element_to_be_clickable(
+            (locators["add_question_btn"][0], locators["add_question_btn"][1]))).click()
+        self.wait.until(EC.element_to_be_clickable(
+            (locators["add_question_btn"][0], locators["add_question_btn"][1])))
 
     def create_textual_question(self, question):
         self.add_new_question()
-        wait_until(self.wait, self.driver, By.CSS_SELECTOR, "div.left.wide mat-slider")
-        text_type_locator = "//*[contains(text(), 'new empty question')]/../../..//div[contains(text(), 'Textual')]/.."
-        self.wait.until(EC.element_to_be_clickable((By.XPATH, text_type_locator))).click()
-        question_field_locator = "//*[contains(text(), 'new empty question')]/../../..//textarea[@placeholder='Question *']"
-        wait_until(self.wait, self.driver, By.XPATH, question_field_locator)
-        type(self.driver, By.XPATH, question_field_locator, question)
+        self.wait.until(EC.element_to_be_clickable(
+            (locators["label_textual"][0], locators["label_textual"][1]))).click()
+        self.wait.until(EC.presence_of_element_located(
+            (locators["question_field_locator"][0], locators["question_field_locator"][1]))).send_keys(question)
 
     def create_single_choice_question(self, question, answer, *args):
         self.add_new_question()
-        wait_until(self.wait, self.driver, By.CSS_SELECTOR, "div.left.wide mat-slider")
-        single_choice_type_locator = "//*[contains(text(), 'new empty question')]/../../..//div[contains(text(), 'Single-Choice')]/.."
-        self.wait.until(EC.element_to_be_clickable((By.XPATH, single_choice_type_locator))).click()
-        question_field_locator = "//*[contains(text(), 'new empty question')]/../../..//textarea[@placeholder='Question *']"
-        wait_until(self.wait, self.driver, By.XPATH, question_field_locator)
-        type(self.driver, By.XPATH, question_field_locator, question)
+        self.wait.until(EC.element_to_be_clickable(
+            (locators["label_single_choice"][0], locators["label_single_choice"][1]))).click()
+        self.wait.until(EC.presence_of_element_located(
+            (locators["question_field_locator"][0], locators["question_field_locator"][1]))).send_keys(question)
         for arg in args:
-            wait_until(self.wait, self.driver, By.XPATH,
-                       "//*[contains(text(), '{}')]/../../..//*[@placeholder='Option {}*']".format(question,
-                                                                                                   args.index(arg) + 1))
-            type(self.driver, By.XPATH, "//*[contains(text(), '{}')]/../../..//*[@placeholder='Option {}*']".format(question,
-                                                                                                               args.index(
-                                                                                                                   arg) + 1),
-                 arg)
+            self.wait.until(EC.visibility_of_element_located(
+                (locators["question_choice_field"][0], locators["question_choice_field"][1].format(question, args.index(arg) + 1)))).send_keys(arg)
             if arg == answer:
-                radio_button = "//*[contains(text(), '{}')]/../../..//*[@placeholder='Option {}*']/../../../../..//mat-radio-button".format(
-                    question, args.index(arg) + 1)
-                click(self.driver, By.XPATH, radio_button)
-            else:
-                continue
+                self.wait.until(EC.presence_of_element_located(
+                    (locators["single_choice_radio_btn"][0], locators["single_choice_radio_btn"][1].format(question,args.index(arg) + 1)))).click()
 
     def create_multiple_choice_question(self, question, answer, *args):
         self.add_new_question()
-        multiple_choice_type_locator = "//*[contains(text(), 'new empty question')]/../../..//div[contains(text(), 'Multiple-Choice')]/.."
-        self.wait.until(EC.element_to_be_clickable((By.XPATH, multiple_choice_type_locator))).click()
-        question_field_locator = "//*[contains(text(), 'new empty question')]/../../..//textarea[@placeholder='Question *']"
-        wait_until(self.wait, self.driver, By.XPATH, question_field_locator)
-        type(self.driver, By.XPATH, question_field_locator, question)
+        self.wait.until(EC.element_to_be_clickable(
+            (locators["label_multi_choice"][0], locators["label_multi_choice"][1]))).click()
+        self.wait.until(EC.presence_of_element_located(
+            (locators["question_field_locator"][0], locators["question_field_locator"][1]))).send_keys(question)
         for arg in args:
-            wait_until(self.wait, self.driver, By.XPATH,
-                       "//*[contains(text(), '{}')]/../../..//*[@placeholder='Option {}*']".format(question,
-                                                                                                   args.index(arg) + 1))
-            type(self.driver, By.XPATH, "//*[contains(text(), '{}')]/../../..//*[@placeholder='Option {}*']".format(question,
-                                                                                                               args.index(
-                                                                                                                   arg) + 1),
-                 arg)
+            self.wait.until(EC.visibility_of_element_located(
+                (locators["question_choice_field"][0],locators["question_choice_field"][1].format(question,args.index(arg) + 1)))).send_keys(arg)
             if arg == answer:
-                check_box = "//*[contains(text(), '{}')]/../../..//*[@placeholder='Option {}*']/../../../../../mat-checkbox".format(
-                    question, args.index(arg) + 1)
-                click(self.driver, By.XPATH, check_box)
-            else:
-                continue
+                self.wait.until(EC.presence_of_element_located(
+                    (locators["multi_choice_ch_box"][0],locators["multi_choice_ch_box"][1].format(question,args.index(arg) + 1)))).click()
 
     def save_quiz(self, quiz_name):
-        by = By.XPATH
-        save_btn = "//button/*[contains(text(),'Save')]"
-        wait_until(self.wait, self.driver, by, save_btn)
-        click(self.driver, by, save_btn)
-        quiz_locator = "//ac-quizzes-list//div[@class = 'quizzes']//*[contains(text(),'{}')]".format(quiz_name)
-        wait_until(self.wait, self.driver, By.XPATH, quiz_locator)
-        element = self.driver.find_element_by_xpath(quiz_locator)
-        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+        self.wait.until(EC.element_to_be_clickable(
+            (locators["save_quiz_btn"][0], locators["save_quiz_btn"][1]))).click()
+        self.wait.until(EC.presence_of_element_located(
+            (locators["quiz_locator"][0], locators["quiz_locator"][1].format(quiz_name))))
 
-    def find_quizz(self, quiz_name):
-
-        quiz_locator = "//ac-quizzes-list//div[@class = 'quizzes']//*[contains(text(),'{}')]".format(quiz_name)
-        wait_until(self.wait, self.driver, By.XPATH, quiz_locator)
-        element = self.driver.find_element_by_xpath(quiz_locator)
+    def find_quiz_in_list_of_quizzes(self, quiz_name):
+        element = self.wait.until(EC.presence_of_element_located(
+            (locators["quiz_locator"][0], locators["quiz_locator"][1].format(quiz_name))))
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
         element.click()
+
+    def get_screenshot(self, quiz_name):
+        self.driver.get_screenshot_as_file('{} created.png'.format(quiz_name))
 
     def logout(self):
         click(self.driver, By.XPATH, "//div[@class='mat-list-item-content']//h5[contains(text(),'Log Out')]")
