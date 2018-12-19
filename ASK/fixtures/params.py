9f6796f0c8e01be03b93c0c5f5ca2e24c194ccb4
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2018-12-18 21:40:20 -0800

    Update params.py

--- ASK/fixtures/params.py
+++ ASK/fixtures/params.py
@@ -1,11 +1,18 @@
 import os
+import random
 
 cwd = os.path.abspath(os.getcwd())
-location = "../browsers/chromedriver"
+location_chrome = "../browsers/chromedriver"
+location_firefox = "../browsers/geckodriver"
 
 DOMAIN = "http://local.school.portnov.com:4520/#"
-BROWSER_TYPE = "Chrome"
-CHROME_EXECUTABLE_PATH = os.path.join(cwd, location)
+browsers = [
+    "chrome",
+    "firefox"
+]
+BROWSER_TYPE = random.choice(browsers)
+CHROME_EXECUTABLE_PATH = os.path.join(cwd, location_chrome)
+FIREFOX_EXECUTABLE_PATH = os.path.join(cwd, location_firefox)
 EXPLICIT_TIMEOUT = 10
 # Just example of some othe timeouts
 # SLOW_TIMEOUT = 30
